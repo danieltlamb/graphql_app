c8e7c543c8eaa176eb499365cacdabffee6b16aa
Daniel Lamb <daniel.lamb@cybergrx.com>
2017-11-07 15:41:59 -0700

    GraphQL App... Learning GraphQL

---
+++ src/index.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import { BrowserRouter } from 'react-router-dom'
+// 1
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { GC_AUTH_TOKEN } from './constants'
+import { ApolloLink, split } from 'apollo-client-preset'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
+// 2
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cj9olv88d0jw801189sdrx4nv' })
+
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(GC_AUTH_TOKEN)
+  const authorizationHeader = token ? `Bearer ${token}` : null
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader
+    }
+  })
+  return forward(operation)
+})
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
+
+// 3
+const client = new ApolloClient({
+  link: httpLinkWithAuthToken,
+  cache: new InMemoryCache()
+})
+
+const wsLink = new WebSocketLink({
+  uri: 'wss://subscriptions.us-west-2.graph.cool/v1/cj9olv88d0jw801189sdrx4nv',
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(GC_AUTH_TOKEN),
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLinkWithAuthToken,
+)
+
+// 4
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
+  , document.getElementById('root')
+)
+registerServiceWorker()
